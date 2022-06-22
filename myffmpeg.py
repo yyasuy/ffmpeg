17206d8117fce34dfa297a1dd0f1466f88b4fe23
Yasuhiro Yamanaka <yamanaka@yasuhironoMacBook-Air.local>
2022-06-22 20:26:48 +0900

    Dynamic ffmpeg path

--- myffmpeg.py
+++ myffmpeg.py
@@ -7,7 +7,10 @@ for i in range( argc ):
 	if i > 0:
 		input_file = sys.argv[ i ]
 		output_file = input_file[:-4] + '-24.mov'
-		command = '/opt/homebrew/Cellar/ffmpeg/4.4_2/bin/ffmpeg -i ' + "'" + input_file + "'" + ' -r 24 -vf scale=-1:720 ' + "'" + output_file + "'"
+		output = subprocess.run( 'which ffmpeg', shell = True, capture_output=True, text=True );
+		ffmpeg_path = output.stdout.strip()
+
+		command = ffmpeg_path + ' -i ' + "'" + input_file + "'" + ' -r 24 -vf scale=-1:720 ' + "'" + output_file + "'"
 
 		print( command )
 
